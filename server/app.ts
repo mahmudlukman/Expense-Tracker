a420006c8a91f92994542c6e4ee09bf9559d5f84
mahmud lukman <mistamann49@gmail.com>
2025-06-23 16:59:11 +0100

    deploy update

--- server/app.ts
+++ server/app.ts
@@ -29,7 +29,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: [
-      "https://codepence-expense-tracker.netlify.app/",
+      "https://codepence-expense-tracker.netlify.app",
       "http://localhost:5173",
     ],
     credentials: true,
--- server/dist/app.js
+++ server/dist/app.js
@@ -27,7 +27,7 @@ exports.app.use((0, cookie_parser_1.default)());
 //cors=>cross origin resource sharing
 exports.app.use((0, cors_1.default)({
     origin: [
-        "https://codepence-expense-tracker.netlify.app/",
+        "https://codepence-expense-tracker.netlify.app",
         "http://localhost:5173",
     ],
     credentials: true,
